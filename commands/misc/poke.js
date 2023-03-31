f2cbd10602a3e842a7efba6f082b1da8427682c0
MeDavoo <thedavidmaddin@gmail.com>
2023-03-31 03:36:02 +0200

    Working Pokemon Trivia Command

--- commands/misc/poke.js
+++ commands/misc/poke.js
@@ -2,14 +2,34 @@ module.exports = {
 	name: "poke",
 	// Refer to typings.d.ts for available properties.
 	async execute (message, args) {
-		let { pg1name } = require("../../pokegen1.json")
 		var linkgif = "https://projectpokemon.org/images/normal-sprite/";
-		var randpokeg1 = pg1name[Math.floor( Math.random() * pg1name.length)];
-        var correctAnswer = randpokeg1;
-        var randpickg1 = linkgif + randpokeg1 + ".gif";
-        message.channel.send(randpickg1);
-		const filter = m => m.author.id === message.author.id;
-        await message.channel.awaitMessages(filter, { max: 1, time: 5000, errors: ['time', 'max']});
-		message.channel.send(correctAnswer);
-	},
+		let { pg1name } = require("../../pokegen1.json")
+		var score = 0
+		pk();
+		
+		function pk()
+		{
+			randpokeg1 = pg1name[Math.floor(Math.random() * pg1name.length)];
+			var randpickg1 = linkgif + randpokeg1 + ".gif";
+	
+			message.reply(randpickg1)
+				.then(() => {
+					const filter = response => {return randpokeg1 === response.content.toLowerCase()};
+					message.channel.awaitMessages({ filter, max: 1, time: 10000, errors: ['time'] })
+						.then(collected => {
+							message.channel.send(`${collected.first().author} **got the correct answer!**`);
+							score++;
+							message.channel.send("**SCORE:** " + score);
+							pk();
+						})
+						.catch(collected => {
+							message.channel.send('You lose!');
+							message.channel.send("**ANSWER:** " + randpokeg1);
+						});
+				});
+		};
+		
+	}
+
+	
 };
