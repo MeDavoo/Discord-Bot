74a82c83de594de1187f68b028212ae12cca5748
MeDavoo <thedavidmaddin@gmail.com>
2023-04-03 07:57:18 +0200

    added gen 4 and 5

--- commands/misc/poke.js
+++ commands/misc/poke.js
@@ -2,16 +2,20 @@ module.exports = {
 	name: "poke",
 	// Refer to typings.d.ts for available properties.
 	async execute(message, args) {
-	  const validGenerations = ["gen1", "gen2", "gen3"]; // list of valid generation inputs
+	  const validGenerations = ["gen1", "gen2", "gen3", "gen4", "gen5"]; // list of valid generation inputs
 	  const generationPrompt = `**Which generation of Pokemon would you like to play with?** [${validGenerations.join(", ")}]`;
 	  var linkgif = "https://projectpokemon.org/images/normal-sprite/";
 	  let { pg1name } = require("../../pokegen1.json")
 	  let { pg2name } = require("../../pokegen2.json")
 	  let { pg3name } = require("../../pokegen3.json")
+	  let { pg4name } = require("../../pokegen4.json")
+	  let { pg5name } = require("../../pokegen5.json")
 	  var scores = {};
 	  var scores2 = {};
 	  var scores3 = {};
-  
+	  var scores4 = {};
+	  var scores5 = {};
+	    
 	  // prompt the user to choose the generation
 	  message.reply(generationPrompt)
 		.then(() => {
@@ -28,7 +32,12 @@ module.exports = {
 				pk2();
 			  } else if (chosenGeneration === "gen3") {
 				pk3();
+			  } else if (chosenGeneration === "gen4") {
+				pk4();
+			  } else if (chosenGeneration === "gen5") {
+				pk5();
 			  }
+			  
 			})
 			.catch(() => {
 			  message.reply("You didn't chose dummy! Try Again.");
@@ -49,7 +58,7 @@ module.exports = {
 				const player = collected.first().author; // get the player who got the correct answer
 				if (!scores[player]) {
 				  // if the player's score is not yet initialized, set it to 0
-				  scores2[player] = 0;
+				  scores[player] = 0;
 				}
 				scores[player]++; // increment the player's score
 				message.channel.send(
@@ -137,8 +146,75 @@ module.exports = {
 		  });
 	  };
 
+	  function pk4() {
+		randpokeg4 = pg4name[Math.floor(Math.random() * pg4name.length)];
+		var randpickg4 = linkgif + randpokeg4 + ".gif";
+  
+		message.reply(randpickg4)
+		  .then(() => {
+			const filter = response => {
+			  return randpokeg4 === response.content.toLowerCase();
+			};
+			message.channel.awaitMessages({ filter, max: 1, time: 10000, errors: ['time'] })
+			  .then(collected => {
+				const player = collected.first().author; // get the player who got the correct answer
+				if (!scores4[player]) {
+				  // if the player's score is not yet initialized, set it to 0
+				  scores4[player] = 0;
+				}
+				scores4[player]++; // increment the player's score
+				message.channel.send(
+				  `${collected.first().author} **got the correct answer! :white_check_mark:**`
+				);
+				message.channel.send(
+				  `**SCORES:** ${Object.entries(scores4)
+					.map(([key, value]) => `${key}: ${value}`)
+					.join(", ")}`
+				); // display the scores of all players
+				pk4();
+			  })
+			  .catch(collected => {
+				message.channel.send('**Loser! :x:**');
+				message.channel.send("**ANSWER:** " + randpokeg4);
+			  });
+		  });
+	  };
 		
-		
+	  function pk5() {
+		randpokeg5 = pg5name[Math.floor(Math.random() * pg5name.length)];
+		var randpickg5 = linkgif + randpokeg5 + ".gif";
+  
+		message.reply(randpickg5)
+		  .then(() => {
+			const filter = response => {
+			  return randpokeg5 === response.content.toLowerCase();
+			};
+			message.channel.awaitMessages({ filter, max: 1, time: 10000, errors: ['time'] })
+			  .then(collected => {
+				const player = collected.first().author; // get the player who got the correct answer
+				if (!scores5[player]) {
+				  // if the player's score is not yet initialized, set it to 0
+				  scores5[player] = 0;
+				}
+				scores5[player]++; // increment the player's score
+				message.channel.send(
+				  `${collected.first().author} **got the correct answer! :white_check_mark:**`
+				);
+				message.channel.send(
+				  `**SCORES:** ${Object.entries(scores5)
+					.map(([key, value]) => `${key}: ${value}`)
+					.join(", ")}`
+				); // display the scores of all players
+				pk5();
+			  })
+			  .catch(collected => {
+				message.channel.send('**Loser! :x:**');
+				message.channel.send("**ANSWER:** " + randpokeg5);
+			  });
+		  });
+	  };	
+
+
 	}
 
 	
